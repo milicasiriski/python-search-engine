78ae59761770e915c7901bcd2f9f22914be40f54
Milica Siriski <milica.siriski998@gmail.com>
2020-02-06 15:51:52 +0100

    #rangirana_pretraga Dodata je funkcija za racunanje ranga stranice kao i pomocna klasa.

---
+++ search_engine/util/rank_calculator.py
@@ -0,0 +1,20 @@
+class RankValues:
+    def __init__(self, value):
+        self._old_value = value
+        self._new_value = 0
+
+    @property
+    def old_value(self):
+        return self._old_value
+
+    @old_value.setter
+    def old_value(self, value):
+        self._old_value = value
+
+    @property
+    def new_value(self):
+        return self._new_value
+
+    @new_value.setter
+    def new_value(self, value):
+        self._new_value = value
