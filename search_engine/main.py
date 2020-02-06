9194f64ae1812bc2b26d49eeb82f85be09a1093a
Luka Doric <luka.doric@uns.ac.rs>
2020-02-06 15:01:56 +0100

    #pretraga_dokumenata u main-u je uradjena pretraga dokumenata koji sadrze unetu rec (uradjena je samo osnovna pretraga za jednu unetu rec)

--- search_engine/main.py
+++ search_engine/main.py
@@ -11,8 +11,8 @@ g = Graph()
 
 def main():
 
-    # path = "C:\\Users\\Luka Doric\\Desktop\\python-2.7.7-docs-html"  # IZMENITI PUTANJU!
-    path = "C:\\Users\\Milica\\Desktop\\python-2.7.7-docs-html"
+    path = "C:\\Users\\Luka Doric\\Desktop\\python-2.7.7-docs-html"  # IZMENITI PUTANJU!
+    # path = "C:\\Users\\Milica\\Desktop\\python-2.7.7-docs-html"
 
     print("Starting search engine \n")
     print("Loading files, please wait.")
@@ -21,6 +21,21 @@ def main():
     end = datetime.now()
     print("Vreme ucitavanja:", end - start)
 
+    while True:
+
+        choice_menu_one = input ("Za pretragu .html dokumenata unesite Y/y\n"
+                                 "za izlazak iz programa unesit X/x.\n")
+
+        if choice_menu_one.lower() == "x":
+            break
+        elif choice_menu_one.lower() == "y":
+            search_word = input("Unesite rec koju zelite da pretrazite:\n")  # osnovna pretraga sa jednom reci
+
+            found, files = t.search(search_word.lower())
+
+            if found:
+                for file in files:
+                    print(file)
 
 def load_files(path):  # ubacivanje reci iz html fajlova
     for file_name in os.listdir(path):
