871a28d166a036dab190af417efefb5b6213c70f
Milica Siriski <milica.siriski998@gmail.com>
2020-02-21 01:09:53 +0100

    #rangirana_pretraga Algoritam za izracunavanje ranga je bolje prilagodjen. Takodje je data dodatna prednost stranicama koje sadrze vise trazenih reci upita.

--- search_engine/main.py
+++ search_engine/main.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 
 from util.graph import Graph
-from util.page_sorter import heapSort
+from util.page_sorter import heapSort, Page
 from util.parserHTML import Parser
 from util.rank_calculator import RankValues
 from util.set import MySet
@@ -14,11 +14,11 @@ all_files = MySet()
 
 
 def main():
-    path = "C:\\Users\\Luka Doric\\Desktop\\python-2.7.7-docs-html"  # IZMENITI PUTANJU!
-    # path = "C:\\Users\\Milica\\Desktop\\python-2.7.7-docs-html"
+    # path = "C:\\Users\\Luka Doric\\Desktop\\python-2.7.7-docs-html"  # IZMENITI PUTANJU!
+    path = "C:\\Users\\Milica\\Desktop\\python-2.7.7-docs-html"
 
     while True:
-        path = input("Unesite korenski direktorijum.\n")
+        #path = input("Unesite korenski direktorijum.\n")
 
         try:
             start = datetime.now()
@@ -67,11 +67,11 @@ def main():
                     found, files = t.search(search_word.lower())
 
                     if found:
-                        calculate_ranks(files)
-                        search_results = files.toList()
+                        search_results = calculate_ranks(files)
                         heapSort(search_results)
-                        for result in search_results:
-                            print("\n", result.path, result.rank)
+                        for i in range(len(search_results)):
+                            path = search_results[i].path
+                            print(str(i), path, "rang:", search_results[i].rank, "linkovi:", len(g.incoming[path]), "reci:", files[path])
 
                 elif choice_menu_two.lower() == "b":
                     search_text = input("Unesite reci koje zelite da pretrazite razdvojene razmakom:\n")
@@ -83,12 +83,21 @@ def main():
                         found, files = t.search(word.lower().strip())
                         result_set = result_set | files
 
-                    calculate_ranks(result_set)
-                    search_results = result_set.toList()
+                    search_results = calculate_ranks(result_set)
+
+                    for page in search_results:
+                        contains_words = 0
+                        for word in search_words:
+                            _, files = t.search(word.lower().strip())
+                            if page.path in files:
+                                contains_words += 1
+                        page.rank *= contains_words
+
                     heapSort(search_results)
 
-                    for result in search_results:
-                        print("\n", result.path, result.rank)
+                    for i in range(len(search_results)):
+                        path = search_results[i].path
+                        print(str(i), path, "rang:", search_results[i].rank, "linkovi:", len(g.incoming[path]), "reci:", result_set[path])
 
                 elif choice_menu_two.lower() == "c":
                     query = input("Unesite dve reci koje zelite odvojene separatorom AND/OR/NOT:\n"
@@ -109,16 +118,16 @@ def main():
                     elif len(length_check) == 2 and length_check[0] == "NOT":
                         search_word = length_check[1].strip()
                         found, files = t.search(search_word.lower())
-                        result_set = all_files
 
                         result_set = all_files - files
 
-                        calculate_ranks(result_set)
-                        search_results = result_set.toList()
+                        search_results = calculate_ranks(result_set)
                         heapSort(search_results)
 
-                        for result in search_results:
-                            print("\n", result.path, result.rank)
+                        for i in range(len(search_results)):
+                            path = search_results[i].path
+                            print(str(i), path, "rang:", search_results[i].rank, "linkovi:", len(g.incoming[path]),
+                                  "reci:", result_set[path])
 
                     elif len(length_check) == 3 and length_check[1] == "OR":
 
@@ -126,13 +135,18 @@ def main():
                         found2, files2 = t.search(length_check[2].lower().strip())
 
                         result_set = files1 | files2
-
-                        calculate_ranks(result_set)
-                        search_results = result_set.toList()
-                        heapSort(search_results)
+                        search_results = calculate_ranks(result_set)
 
                         for result in search_results:
-                            print("\n", result.path, result.rank)
+                            if result.path in files1 and result.path in files2:
+                                result.rank *= 2
+
+                        heapSort(search_results)
+
+                        for i in range(len(search_results)):
+                            path = search_results[i].path
+                            print(str(i), path, "rang:", search_results[i].rank, "linkovi:", len(g.incoming[path]),
+                                  "reci:", result_set[path])
 
                     elif len(length_check) == 3 and length_check[1] == "NOT":
 
@@ -141,12 +155,13 @@ def main():
 
                         result_set = files1 - files2
 
-                        calculate_ranks(result_set)
-                        search_results = result_set.toList()
+                        search_results = calculate_ranks(result_set)
                         heapSort(search_results)
 
-                        for result in search_results:
-                            print("\n", result.path, result.rank)
+                        for i in range(len(search_results)):
+                            path = search_results[i].path
+                            print(str(i), path, "rang:", search_results[i].rank, "linkovi:", len(g.incoming[path]),
+                                  "reci:", result_set[path])
 
                     elif len(length_check) == 3 and length_check[1] == "AND":
 
@@ -155,12 +170,13 @@ def main():
 
                         result_set = files1 & files2
 
-                        calculate_ranks(result_set)
-                        search_results = result_set.toList()
+                        search_results = calculate_ranks(result_set)
                         heapSort(search_results)
 
-                        for result in search_results:
-                            print("\n", result.path, result.rank)
+                        for i in range(len(search_results)):
+                            path = search_results[i].path
+                            print(str(i), path, "rang:", search_results[i].rank, "linkovi:", len(g.incoming[path]),
+                                  "reci:", result_set[path])
 
                     else:
                         print("Nije ispravno unet upit."
@@ -182,30 +198,47 @@ def load_files(path):  # ubacivanje reci iz html fajlova
 
 
 def calculate_ranks(search_results: MySet, max_iter=100):
+    if len(search_results) == 0:
+        return []
     pages = {}  # key - putanja fajla, value - RankValues sluzi za racunanje ukupnog ranga
-    total = search_results.words_in_total()  # ukupan broj reci u skupu
-
-    # ucitavanje stranica u tabelu i postavljanje pocetnih vrednosti srazmerno broju ponavljanja reci u fajlu
+    total = search_results.words_in_total  # ukupan broj reci u skupu
+    average = search_results.words_on_average  # prosecan broj reci u skupu
+    n = len(g.outgoing)
+    d = 0.85  # damping factor
+    # ucitavanje stranica u tabelu i postavljanje pocetnih vrednosti
     for page in g.outgoing.keys():
-        if page in search_results.elements:
-            value = float(search_results.elements[page] / total)
-        else:
-            value = 0
-        pages[page] = RankValues(value)
-
+        pages[page] = RankValues(float(1 / total))
     # racunanje ranga
+    # klasican PageRank algoritam racuna rang stranice u odnosu na broj linkova koji na nju ukazuju kao i njihove
+    # "jacine"
     for i in range(max_iter):
         for page in pages.keys():
             if g.num_of_outgoing(page) != 0:
                 value = float(pages[page].old_value / g.num_of_outgoing(page))
+                if page in search_results:
+                    value *= 1.2
+                else:
+                    value *= 0.8
                 for link in g.outgoing[page]:
                     pages[link].new_value += value
 
+        # d - faktor prigusenja, sprecava velike oscilacije u vrednostima
+        # na novu vrednost ranga stranice dodaje se broj reci koji sadrzi ta stranica u odnosu na prosecan broj trazene
+        # reci po stranici kako bi se povecao uticaj linka koji sadrzi tu rec u odnosu na linkove koji ne sadrze rec
         for page in pages:
-            pages[page].old_value, pages[page].new_value = pages[page].new_value, 0
+            if page in search_results:
+                pages[page].old_value = (1-d) + d*(pages[page].new_value + (search_results[page] / average))
+            else:
+                pages[page].old_value = (1 - d) + d * pages[page].new_value
+            pages[page].new_value = 0
 
-    for page in search_results.elements:
-        search_results.elements[page] = pages[page].old_value
+    final_list = []
+    for page in pages:
+        # konacan rang se dobija mnozenjem sa brojem ponavljanja reci u odnosu na prosecan broj ponavljanja reci kako
+        # bi se uzeo u obzir uticaj samog broja reci koji sadrzi stranica
+        if page in search_results:
+            final_list.append(Page(page, pages[page].old_value))
+    return final_list
 
 
 main()
